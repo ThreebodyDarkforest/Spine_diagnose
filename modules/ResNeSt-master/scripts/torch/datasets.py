5a6da7559bf7c1b44cf50326fa8492b8bf5ab99f
marcus <tangjzh23@mail2.sysu.edu.cn>
2023-06-20 12:49:34 +0800

    launch new dev feature: resnest

---
+++ modules/ResNeSt-master/scripts/torch/datasets.py
@@ -0,0 +1,62 @@
+import glob, os, ast
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+
+data_transform = {
+    'train': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomRotation((-30, 30)),
+        transforms.Lambda(lambda x: add_gaussian_noise(x, mean=0, std=0.1)),
+        #transforms.RandomVerticalFlip(),
+        transforms.RandomAutocontrast(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+def add_gaussian_noise(img, mean=0, std=1):
+    noise = np.random.randn(img.size[0], img.size[1], 3) * std + mean
+    noisy_img = np.array(img, dtype=np.float64) + noise
+    return Image.fromarray(np.uint8(noisy_img))
+
+class SpineDataSet(Dataset):
+    def __init__(self, path, transforms=None):
+        data_paths = sorted(glob.glob(os.path.join(path, '**.jpg')))
+        anno_paths = sorted(glob.glob(os.path.join(path, '**.txt')))
+        self.transforms = transforms
+        self.data_info = []
+        for pair in zip(data_paths, anno_paths):
+            img_path = pair[0]
+            anno_path = pair[1]
+            with open(anno_path, 'r') as f:
+                ret = f.readline()
+                disease_type = ast.literal_eval(ret[2:])
+                if len(disease_type) == 0: disease_type.extend([5, 5])
+                if len(disease_type) <= 1: disease_type.append(5)
+                self.data_info.append([img_path, torch.tensor(disease_type, dtype=torch.long)])
+
+    def __len__(self) -> int:
+        return len(self.data_info)
+
+    def __getitem__(self, idx):
+        img_path, target = self.data_info[idx]
+        data = Image.open(img_path).convert('RGB')
+        if self.transforms is not None:
+            data = self.transforms(data)
+        return data, target
+
+
+def get_dataloader(img_dir, batch_size: int = 32, dtype=None, num_workers: int = 0):
+    transform = data_transform['val'] if dtype == 'val' else data_transform['train']
+    dataset = SpineDataSet(img_dir, transform)
+    sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+    return DataLoader(dataset, batch_size, num_workers=num_workers, pin_memory=True, sampler=sampler), sampler
+
